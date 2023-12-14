d148db2ec1bbdf663a584db07dcac89b485f7a31
Rstyk <ros.tkah@gmail.com>
2023-12-14 22:07:37 +0200

    LAB 11.2 A

---
+++ laba 11.2A/laba 11.2A.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace laba112A
+{
+	TEST_CLASS(laba112A)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
